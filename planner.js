3a61e6f657c3cce789ca5c97e3d836d225b61c1d
Francis Roy Balasabas <francisroy1124@gmail.com>
2020-10-17 21:58:54 -0400

    downloaded moment.js library and linked to html

--- planner.js
+++ planner.js
@@ -1,5 +1,3 @@
-var writeToSchedule = "currentDay";
-
 $(document).ready(function () {
     $("#currentDay").text(moment().format('LLLL'));
     writeToSchedule();
@@ -13,4 +11,68 @@ $("#saveBtnNine").click(function () {
     bookAppointment(appointmentTimeSlot, appointmentDetails);
     writeToSchedule();
     console.log(appointmentTimeSlot); // THis works pretty well
+});
+
+$("#saveBtnTen").click(function () {
+    //console.log("user clicked to save 10am appointment");
+    var appointmentTimeSlot = "#textareaTen";
+    var appointmentDetails = document.getElementById('textareaTen').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnEleven").click(function () {
+    //console.log("user clicked to save 11am appointment");
+    var appointmentTimeSlot = "#textareaEleven";
+    var appointmentDetails = document.getElementById('textareaEleven').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnTwelve").click(function () {
+    //console.log("user clicked to save 12pm appointment");
+    var appointmentTimeSlot = "#textareaTwelve";
+    var appointmentDetails = document.getElementById('textareaTwelve').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnOne").click(function () {
+    //console.log("user clicked to save 1pm appointment");
+    var appointmentTimeSlot = "#textareaOne";
+    var appointmentDetails = document.getElementById('textareaOne').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnTwo").click(function () {
+    //console.log("user clicked to save 2pm appointment");
+    var appointmentTimeSlot = "#textareaTwo";
+    var appointmentDetails = document.getElementById('textareaTwo').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnThree").click(function () {
+    //console.log("user clicked to save 3pm appointment");
+    var appointmentTimeSlot = "#textareaThree";
+    var appointmentDetails = document.getElementById('textareaThree').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnFour").click(function () {
+    //console.log("user clicked to save 4pm appointment");
+    var appointmentTimeSlot = "#textareaFour";
+    var appointmentDetails = document.getElementById('textareaFour').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
+});
+
+$("#saveBtnFive").click(function () {
+    //console.log("user clicked to save 5pm appointment");
+    var appointmentTimeSlot = "#textareaFive";
+    var appointmentDetails = document.getElementById('textareaFive').value;
+    bookAppointment(appointmentTimeSlot, appointmentDetails);
+    writeToSchedule();
 });
